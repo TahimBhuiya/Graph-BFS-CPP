f68700a6824c894b6739aea379abeaed3fbb6607
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-11-18 18:22:59 -0500

    Added onto BFS function

--- bfs-graph.cpp
+++ bfs-graph.cpp
@@ -22,7 +22,7 @@ void BFS(int start, vector<vector<int>>& graph) {
         // Traverse all neighbors
         for (int neighbor : graph[node]) {
             if (!visited[neighbor]) {   // Visit only if unvisited
-                
+                visited[neighbor] = true;
                       
             }
         }
